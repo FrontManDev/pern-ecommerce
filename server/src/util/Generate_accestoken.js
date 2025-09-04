c1f5735d716865168307cda4442d3fe00a920dc4
Hamida AbdelKarim <hamidakaim67@example.com>
2025-09-04 04:02:46 -0700

    add comment for mor details

--- server/src/util/Generate_accestoken.js
+++ server/src/util/Generate_accestoken.js
@@ -1,5 +1,8 @@
+// function fo generate the accestoken
 function Generate_accestoken(id,role) {
+    // import JWT module
     const JWT = require('jsonwebtoken');
+    // generate and return token 
     return JWT.sign({ userId: id, Role: role }, process.env.ACCESTOKEN_SECRET_KEY, { expiresIn: process.env.ACCESTOKEN_EXSPIRE_DATE });
 }
 
