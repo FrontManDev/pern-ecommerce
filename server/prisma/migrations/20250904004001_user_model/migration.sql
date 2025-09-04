39dfb167debe374dfbe4e6f80ef33dfd25afd3a4
Hamida AbdelKarim <hamidakaim67@example.com>
2025-09-03 17:40:53 -0700

    migrate the schema.prisma first time

---
+++ server/prisma/migrations/20250904004001_user_model/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "image" TEXT,
+    "role" "public"."Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
